59bcecfec36c7827a7d8516cd9105abdf625a43d
Tatsuto YAMAMOTO <me@laminne33569.net>
2024-07-31 18:10:55 +0900

    feat: setup storybook, radix ui

---
+++ .storybook/main.ts
@@ -0,0 +1,24 @@
+import type { StorybookConfig } from "@storybook/react-vite";
+
+const config: StorybookConfig = {
+  stories: ["../app/**/*.stories.@(js|jsx|mjs|ts|tsx)", "../app/**/*.mdx"],
+  addons: [
+    "@storybook/addon-onboarding",
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@chromatic-com/storybook",
+    "@storybook/addon-interactions"
+  ],
+  framework: {
+    name: "@storybook/react-vite",
+    options: {
+      builder: {
+        viteConfigPath: "vite-sb.config.ts"
+      }
+    }
+  },
+  docs: {
+    autodocs: "tag"
+  }
+};
+export default config;
