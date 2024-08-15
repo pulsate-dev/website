b10d69e2d19c42c3011bfe83e512bcaa6b2cba8e
m1sk9 <me@m1sk9.dev>
2024-08-16 03:45:28 +0900

    feat: Add SEO settings

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image';
 import { useRouter } from 'next/router';
-import { DocsThemeConfig } from 'nextra-theme-docs';
+import {DocsThemeConfig, useConfig} from 'nextra-theme-docs';
 
 const repositoryURL = 'https://github.com/pulsate-dev/docs';
 
@@ -11,11 +11,11 @@ const themeConfig: DocsThemeConfig = {
     link: 'https://link.pulsate.dev/discord'
   },
   i18n: [
-    { locale: 'ja', text: '日本語' },
+    { locale: 'ja', text: 'Japanese' },
     { locale: 'en', text: 'English' }
   ],
   editLink: {
-    text: 'Edit this page'
+    text: 'Edit this page on GitHub',
   },
   project: {
     link: 'https://github.com/pulsate-dev/pulsate'
@@ -35,8 +35,59 @@ const themeConfig: DocsThemeConfig = {
       return `${repositoryURL}/issues/new`;
     }
   },
+  logo: (
+    <>
+      <Image src={'/logo.png'} alt={"Pulsate's logo"} width={40} height={40}/>
+      <span style={{ marginLeft: '10px', fontWeight: 500 }}>Pulsate Docs</span>
+    </>
+  ),
+  head: () => {
+    const {asPath, defaultLocale, locale} = useRouter();
+    const { frontMatter } = useConfig();
+    const url =
+      'https://docs.pulsate.dev' +
+      (defaultLocale === locale ? asPath : `/${locale}${asPath}`)
+
+    return (
+      <>
+        <meta charSet="utf-8"/>
+        <meta name="description" content={frontMatter.description || 'Documentation for the Pulsate Project'}/>
+        <meta name="og:title" content={frontMatter.title || 'Pulsate Docs'}/>
+        <meta property="og:type" content="website"/>
+        <meta property="og:locale" content="ja"/>
+        <meta property="og:description" content={frontMatter.description || 'Documentation for the Pulsate Project'}/>
+        <meta property="og:site_name" content={frontMatter.title || 'Pulsate Docs'}/>
+        <meta property="og:url" content={url}/>
+        <meta name="theme-color" content="#04CDFF"/>
+
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicons/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicons/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicons/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/favicons/site.webmanifest"/>
+        <link
+          rel="mask-icon"
+          href="/favicons/safari-pinned-tab.svg"
+          color="#5bbad5"
+        />
+      </>
+    )
+  },
   useNextSeoProps() {
-    const { asPath } = useRouter();
+    const {asPath} = useRouter();
     if (asPath !== '/') {
       return {
         titleTemplate: '%s | Pulsate Docs'
