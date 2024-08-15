0e517a0f01cb34b2459997032c746eace5a0e177
m1sk9 <me@m1sk9.dev>
2024-08-16 03:31:58 +0900

    feat: Add Navgation components

---
+++ components/navigation.tsx
@@ -0,0 +1,23 @@
+import { Fragment } from "react";
+
+interface NavigationProps {
+  key: string;
+  href: string;
+}
+
+const NavigationItems: NavigationProps[] = [
+  { key: "Getting Started", href: "/getting-started" },
+]
+
+export default function Navigation() {
+  return (
+    <nav>
+      {NavigationItems.map((item, index) => (
+        <Fragment key={index}>
+          <a href={item.href}>{item.key}</a>
+          {index < NavigationItems.length - 1 && " ・ "}
+        </Fragment>
+      ))}
+    </nav>
+  );
+}
